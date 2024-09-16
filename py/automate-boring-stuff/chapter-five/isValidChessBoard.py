0b7a0c01293751a667e7ce68eac826c544311883
neczju <neczju@tuta.com>
2024-09-16 14:44:23 +0200

    chapter five summary

--- py/automate-boring-stuff/chapter-five/isValidChessBoard.py
+++ py/automate-boring-stuff/chapter-five/isValidChessBoard.py
@@ -1,45 +1,66 @@
-import pprint
-
-
-def validPositionGenerator():
-    pos = []
+def validNotationGenerator():
+    validNotation = []
     for x in range(9):
         for y in 'abcdefgh':
-            pos.append(str(x + 1) + y)
-    return pos
+            validNotation.append(str(x + 1) + y)
+    return tuple(validNotation)
 
 
 def validChessBoardGenerator():
-    validChessBoard = {'wking': {'amount': 1,
-                                 'validPos': validPositionGenerator()},
-                       'bking': {'amount': 1,
-                                 'validPos': validPositionGenerator()},
-                       'wqueen': {'amount': 1,
-                                  'validPos': validPositionGenerator()},
-                       'bqueen': {'amount': 1,
-                                  'validPos': validPositionGenerator()},
-                       'wknight': {'amount': 2,
-                                   'validPos': validPositionGenerator()},
-                       'bknight': {'amount': 2,
-                                   'validPos': validPositionGenerator()},
-                       'wbishop': {'amount': 2,
-                                   'validPos': validPositionGenerator()},
-                       'bbishop': {'amount': 2,
-                                   'validPos': validPositionGenerator()},
-                       'wrook': {'amount': 2,
-                                 'validPos': validPositionGenerator()},
-                       'brook': {'amount': 2,
-                                 'validPos': validPositionGenerator()},
-                       'wpawn': {'amount': 8,
-                                 'validPos': validPositionGenerator()},
-                       'bpawn': {'amount': 8,
-                                 'validPos': validPositionGenerator()}
-                       }
+    validChessBoard = {}
+    figureName = ('king', 'queen', 'knight', 'bishop', 'rook', 'pawn')
+    figurePrefix = ('w', 'b')
+    for p in figurePrefix:
+        for n in figureName:
+            if n == 'king':
+                validChessBoard.setdefault(p + n, 1)
+            elif n == 'queen':
+                validChessBoard.setdefault(p + n, 1)
+            elif n in ['knight', 'bishop', 'rook']:
+                validChessBoard.setdefault(p + n, 2)
+            elif n == 'pawn':
+                validChessBoard.setdefault(p + n, 8)
+    validChessBoard.setdefault('', 64)
     return validChessBoard
 
 
 def isValidChessBoard(validBoard, board):
-    # TODO: this will be complicated so for tommorow
-    return None
+    validNotation = validNotationGenerator()
+    for notation, figure in board.items():
+        # notation check
+        if notation not in validNotation:
+            print('There is no ' + notation + ' position on the chessboard.')
+            return False
+        # piece name check
+        elif figure not in validBoard.keys():
+            print('There is no ' + figure + ' piece in chess.')
+            return False
 
-# TODO: testBoard = {}
+        # checking if amount of figure on board is greater than validAmount
+        amount = 0
+        for currentFigure in board.values():
+            if currentFigure == figure:
+                amount += 1
+        for validFigure, validAmount in validBoard.items():
+            if figure == validFigure and amount > validAmount:
+                print('Too many ' + figure +
+                      ' pieces on the chessboard, amount '
+                      + str(amount) + ' expected ' + str(validAmount) + '.')
+                return False
+
+    return True
+
+
+testBoard = {'1a': 'bking', '2a': 'bqueen', '3a': 'brook', '4a': 'brook',
+             '5a': 'bknight', '6a': 'bknight', '7a': 'bbishop', '8a': 'bbishop',
+             '1b': 'bpawn', '2b': 'bpawn', '3b': 'bpawn', '4b': 'bpawn',
+             '5b': 'bpawn', '6b': 'bpawn', '7b': 'bpawn', '8b': 'bpawn',
+             '1c': 'wking', '2c': 'wqueen', '3c': 'wrook', '4c': 'wrook',
+             '5c': 'wbishop', '6c': 'wbishop', '7c': 'wknight', '8c': 'wknight',
+             '1e': 'wpawn', '2e': 'wpawn', '3e': 'wpawn', '4e': 'wpawn',
+             '5e': 'wpawn', '6e': 'wpawn', '7e': 'wpawn', '8e': 'wpawn',
+             '1f': '', '2f': '', '3f': '', '4f': '', '5f': '', '6f': '',
+             '7f': '', '8f': '', '1g': '', '2g': '', '3g': '', '4g': '',
+             '5g': '', '6g': '', '7g': '', '8g': '', '1h': '', '2h': '',
+             '3h': '', '4h': '', '5h': '', '6h': '', '7h': '', '8h': ''}
+print(isValidChessBoard(validChessBoardGenerator(), testBoard))
