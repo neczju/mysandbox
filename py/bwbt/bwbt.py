1955c866b23edd039469ce0946d6635b95d5819a
neczju <neczju@tuta.com>
2024-09-27 21:29:46 +0200

    updated bwbt

--- py/bwbt/bwbt.py
+++ py/bwbt/bwbt.py
@@ -1,15 +1,30 @@
 #! python3
-# bwbt.py -- tool for backing up my minecraft beta worlds
+# bwbt.py -- tool for backing up my minecraft beta and bta worlds
 
 import subprocess
 import time
 import os
-import os.path
+import sys
 
 home_dir = os.environ['HOME']
-source_dir = home_dir + '/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/b1.7.3/.minecraft/saves/'
 target_dir = 'backups'
-backup_dir_name = 'beta_worlds'
+
+if len(sys.argv) < 2:
+    print('Type an argument!')
+    sys.exit()
+
+argument = sys.argv[1]
+
+if argument == 'beta': # beta backup
+    source_dir = home_dir + '/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/b1.7.3/.minecraft/saves/'
+    backup_dir_name = 'beta_worlds'
+elif argument == 'bta': # better than adventure backup
+    source_dir = home_dir + '/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/BTA_MANAGED_INSTANCE/.minecraft/saves/'
+    backup_dir_name = 'bta_worlds'
+else:
+    print('No argument named ' + argument + '!')
+    sys.exit()
+
 
 # check if target directory not exist
 if not os.path.exists(target_dir):
