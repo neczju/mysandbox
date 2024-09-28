b7a7ef3e68c0975bf9fb78f70008edab60195f62
neczju <neczju@tuta.com>
2024-09-28 21:05:42 +0200

    updated bwbt

--- py/bwbt/bwbt.py
+++ py/bwbt/bwbt.py
@@ -5,6 +5,7 @@ import subprocess
 import time
 import os
 import sys
+import shutil
 
 home_dir = os.environ['HOME']
 target_dir = 'backups'
@@ -15,12 +16,15 @@ if len(sys.argv) < 2:
 
 argument = sys.argv[1]
 
+world_dir = ['/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/b1.7.3/.minecraft/saves/',
+             '/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/bta_babric_instance_7.2_01/.minecraft/saves/']
+
 if argument == 'beta': # beta backup
-    source_dir = home_dir + '/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/b1.7.3/.minecraft/saves/'
-    backup_dir_name = 'beta_worlds'
+    source_dir = home_dir + world_dir[0]
+    backup_filename = 'beta_worlds'
 elif argument == 'bta': # better than adventure backup
-    source_dir = home_dir + '/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/BTA_MANAGED_INSTANCE/.minecraft/saves/'
-    backup_dir_name = 'bta_worlds'
+    source_dir = home_dir + world_dir[1]
+    backup_filename = 'bta_worlds'
 else:
     print('No argument named ' + argument + '!')
     sys.exit()
@@ -33,8 +37,7 @@ if not os.path.exists(target_dir):
 # checks if source direcotry exist and creates backup with current localtime
 if os.path.exists(source_dir):
     localtime_format = time.strftime('_%d_%m_%Y_%H_%M_%S', time.localtime())
-    subprocess.run(['cp', '-r', source_dir, target_dir + '/' + backup_dir_name + localtime_format])
-    if subprocess.CompletedProcess:
-        print('Backup successfuly created in %s directory' % (target_dir))
+    os.chdir(target_dir)
+    shutil.make_archive(backup_filename + localtime_format, 'zip', source_dir) # makes zip archive file of saves directory
 else:
     print('Source directory does not exist :(')
---
+++ py/testmkarch/test.py
@@ -0,0 +1,3 @@
+import shutil
+
+shutil.make_archive('test', 'zip', 'test1')
